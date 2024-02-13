5a866ff310b5d2a26682878380bf21da13009d12
DiegoMulder <113519714+DiegoMulder@users.noreply.github.com>
2024-02-13 14:29:56 +0100

    Pushie

--- GameJam2024/Assets/OwnScripts/Gems/GemBehaviour.cs
+++ GameJam2024/Assets/OwnScripts/Gems/GemBehaviour.cs
@@ -7,7 +7,9 @@ public class GemBehaviour : MonoBehaviour
     public GameObject wand_Object, gem_Object;
     public Transform gemWandOrigin;
     private bool placedGemCheck = false;
-    private float distance;
+    //private float distance;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,17 +19,27 @@ public class GemBehaviour : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        distance = Vector3.Distance(gem_Object.transform.position, wand_Object.transform.position);
-        GemLogic();
+        //distance = Vector3.Distance(gem_Object.transform.position, wand_Object.transform.position);
+        //GemLogic();
     }
 
-    private void GemLogic()
+	private void OnTriggerEnter(Collider other)
 	{
-        if (!placedGemCheck && distance <= 0.2f)
+		if (other.gameObject.CompareTag("gemOrigin") && !placedGemCheck)
 		{
-            gem_Object.transform.position = new Vector3(gemWandOrigin.position.x, gemWandOrigin.position.y, gemWandOrigin.position.z);
-            placedGemCheck = true;
+			gem_Object.transform.position = new Vector3(gemWandOrigin.position.x, gemWandOrigin.position.y, gemWandOrigin.position.z);
+			placedGemCheck = true;
 		}
-        else if(placedGemCheck && distance > 0.2) placedGemCheck = false;
-    }
+		else placedGemCheck = false;
+	}
+
+	//   private void GemLogic()
+	//{
+	//       if (!placedGemCheck && distance <= 0.2f)
+	//	{
+	//           gem_Object.transform.position = new Vector3(gemWandOrigin.position.x, gemWandOrigin.position.y, gemWandOrigin.position.z);
+	//           placedGemCheck = true;
+	//	}
+	//       else if(placedGemCheck && distance > 0.2) placedGemCheck = false;
+	//   }
 }
