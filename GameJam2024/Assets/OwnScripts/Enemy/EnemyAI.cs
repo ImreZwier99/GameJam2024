c2d4f677ebbb7776b911b56c9db148a6a1174c77
ImreZwier99 <ifrzwiers@st.noorderpoort.nl>
2024-02-14 17:40:36 +0100

    added ragdoll and health system

--- GameJam2024/Assets/OwnScripts/Enemy/EnemyAI.cs
+++ GameJam2024/Assets/OwnScripts/Enemy/EnemyAI.cs
@@ -5,29 +5,43 @@ using UnityEngine.AI;
 
 public class EnemyAI : MonoBehaviour
 {
-    public Transform Player;
-    public NavMeshAgent Enemy;
-    public float Speed;
+    public GameObject Player;
+    public GameObject Enemy;
+    public float Speed = 0.001f;
     public bool isDead;
     public int Health = 50;
+    Collider[] colliders;
+    public GameObject enemy;
     void Update()
     {
-        Enemy.SetDestination(Player.position);
-        EnemyDead();
-        Health = 50;
+        Enemy.transform.position = Vector3.MoveTowards(Enemy.transform.position, Player.transform.position, Speed);
+        //Enemy.SetDestination(Player.position);
+        //Health = 50;
+        colliders = enemy.GetComponentsInChildren<Collider>();
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "weapon")
+        {
+            Health -= 25;
+            EnemyDead();
+        }
     }
     public void EnemyDead()
     {
-        //if (Health == 0)
-        //{
-        //    isDead = true;
-        //}
-        
-        if (isDead == true)
+        if (Health == 0)
         {
-            GetComponent<NavMeshAgent>().speed = 0;
+            //GetComponent<NavMeshAgent>().speed = 0;
+            Speed = 0;
             GetComponent<Animator>().enabled = false;
             GetComponent<EnemyDieTimer>().enabled = true;
+            GetComponent<BoxCollider>().enabled = false;
+
+            foreach (Collider col in colliders)
+            {
+                col.enabled = true;
+            }
         }
     }
 }
