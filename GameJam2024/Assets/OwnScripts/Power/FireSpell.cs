a5764c6f3b749a84efbf86442ce155517d769f8f
RocketMarvin <jonkermarvin1@gmail.com>
2024-02-15 21:13:01 +0100

    d

--- GameJam2024/Assets/OwnScripts/Power/FireSpell.cs
+++ GameJam2024/Assets/OwnScripts/Power/FireSpell.cs
@@ -126,7 +126,7 @@ public class FireSpell : MonoBehaviour
         GameObject currentBullet = Instantiate(spell);
         //rotate bullet to shoot direction
         currentBullet.transform.position = attackpoint.position;
-        currentBullet.GetComponent<Rigidbody>().velocity = attackpoint.forward * shootforce;
+        currentBullet.GetComponent<Rigidbody>().velocity = attackpoint.up * shootforce;
         Destroy(currentBullet, 5);
 
         //add force
